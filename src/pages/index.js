25a2d5525f9d488da9c183068bfceeaf15116d0e
Kyle Mathews <mathews.kyle@gmail.com>
2021-01-16 13:54:23 -0800

    Add example of using css modules

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,1 +1,5 @@
-export default () => "This is red in develop but white in production!";
+import * as React from "react";
+
+export default () => (
+  <div className="red">This is red in develop but white in production!</div>
+);
--- src/pages/red.js
+++ src/pages/red.js
@@ -1,2 +1,7 @@
-import '../styles/red.css'
-export default () => "This is red both in develop and in production.";
+import * as React from "react";
+import styles from "../styles/red.module.css";
+export default () => (
+  <div className={styles.red}>
+    This is red both in develop and in production.
+  </div>
+);
